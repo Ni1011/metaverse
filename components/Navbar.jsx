bef3c75079406ac24816d2c1873658342fa2c662
Nitesh Agarwal <niteshkumar.nk649@gmail.com>
2023-07-20 12:16:58 +0530

    deploy change

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -15,20 +15,20 @@ const Navbar = () => (
     <div className="absolute w-[50%] inset-0 gradient-01" />
     <div className={`${styles.innerWidth} mx-auto flex justify-between gap-8`}>
       <img
-        src="/search.svg"
+        src="./search.svg"
         alt="search"
         className="w-[24px] h-[24px] object-contain"
       />
       <h2 className="font-extrabold text-[24px] leading-[30px] text-white flex items-center ">
         <img
-          src="/favicon.svg"
+          src="./favicon.svg"
           alt="icon"
           className="w-[45px] h-[45px] object-contain"
         />
         ETAVERSE
       </h2>
       <img
-        src="/menu.svg"
+        src="./menu.svg"
         alt="menu"
         className="w-[24px] h-[24px] object-contain "
       />
